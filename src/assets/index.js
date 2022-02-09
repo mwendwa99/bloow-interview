5cd27f31feb8414da8e5389cacb49c9f2fd1c012
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2022-02-09 20:29:32 +0300

    ui updates done

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-anonymous-default-export */
 export default {
     landing: require('./landing.png'),
+    logo: require('./logo.png'),
 }
--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,45 +1,44 @@
 import React from 'react';
 import assets from '../../assets';
 // assets
-import images from '../../assets';
+import '../../assets';
 
 export default function Header() {
     return (
-        <main className="dark:bg-gray-800 bg-white">
-            <section className="text-gray-400 bg-gray-900 body-font">
-                <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-                    <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-                            Come and dance,
-                            <br className="hidden lg:inline-block" />
-                            rewind your
-                            <span className="font-caveat italic  dark:text-pink-500">
-                                &nbsp; hapiness
-                            </span>
-                        </h1>
-                        <p className="mb-8 leading-relaxed">
-                            Music that speaks to the heart in ways words cannot express.
-                            <br />
-                            There Is No Sore It Will Not Heal, No App It Will Not Subdue.
-                        </p>
-                        <div className="flex justify-center">
-                            <button className="inline-flex text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded text-lg">
-                                Get Started
-                            </button>
-                            <button className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-                                Learn More
-                            </button>
-                        </div>
-                    </div>
-                    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-                        <img
-                            className="object-cover object-center rounded"
-                            alt="hero"
-                            src={assets.landing}
-                        />
+        // <main className="h-screen">
+        <section className="text-gray-400 h-screen bg-gray-900 body-font">
+            <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+                <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                    <h1 className="title-font sm:text-4xl text-3xl md:text-6xl mb-4 font-medium text-white">
+                        Come and dance,
+                        <br className="hidden lg:inline-block" />
+                        rewind your
+                        <span className="font-caveat italic  dark:text-pink-500">
+                            &nbsp;happiness
+                        </span>
+                    </h1>
+                    <p className="mb-8 sm:text-sm md:text-2xl leading-relaxed">
+                        Music that speaks to the heart in ways words cannot express.
+                        There Is No Sore It Will Not Heal, No App It Will Not Subdue.
+                    </p>
+                    <div className="flex justify-center">
+                        <button className="inline-flex text-white rounded-full bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 text-lg">
+                            Get Started
+                        </button>
+                        <button className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded-full text-lg">
+                            Learn More
+                        </button>
                     </div>
                 </div>
-            </section>
-        </main>
+                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                    <img
+                        className="object-cover object-center rounded"
+                        alt="hero"
+                        src={assets.landing}
+                    />
+                </div>
+            </div>
+        </section>
+        // </main>
     )
 }
